d9b8d592c2fbd925428c9701166fa1a3c6d85aad
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-05 09:49:00 -0400

    sloppy options

--- src/Data/Viz/Backend/GraphViz/Map.hs
+++ src/Data/Viz/Backend/GraphViz/Map.hs
@@ -1,13 +1,15 @@
 -- :( lmfao
 module Data.Viz.Backend.GraphViz.Map (
  mapTo'SimpleGraph,
- listTo'SimpleGraph
+ listTo'SimpleGraph,
+ lr,
+ tb
 ) where
 
 import qualified Data.Map as M
 
-mapTo'SimpleGraph :: String -> M.Map String [String] -> String
-mapTo'SimpleGraph t m = "digraph g {\n" ++ title t ++ concatenated ++ "\n}"
+mapTo'SimpleGraph :: String -> String -> M.Map String [String] -> String
+mapTo'SimpleGraph t opts m = "digraph g {\n" ++ title t ++ opts ++ concatenated ++ "\n}"
  where
   keys = M.keys m
   nodes =
@@ -18,9 +20,12 @@ mapTo'SimpleGraph t m = "digraph g {\n" ++ title t ++ concatenated ++ "\n}"
     (\elem -> elem) $ (M.!) m key
   concatenated = concat $ concat nodes
 
-listTo'SimpleGraph :: String -> [(String,[String])] -> String
-listTo'SimpleGraph t m = "digraph g {\n" ++ title t ++ concatenated ++ "}\n"
+listTo'SimpleGraph :: String -> String -> [(String,[String])] -> String
+listTo'SimpleGraph t opts m = "digraph g {\n" ++ title t ++ opts ++ concatenated ++ "}\n"
  where
   concatenated = concat $ concat $ map (\(key,rest) -> map (\el -> "\""++key++"\"->\""++el ++"\";\n") rest) m
 
 title t = "graph [label=\""++ t ++"\", labelloc=t, fontsize=30];\n"
+common opts = concat $ map (++ "\n") $ ["overlap=false;","bgcolor=transparent;","splines=true;"] ++ opts
+lr = common ["rankdir=LR;"]
+tb = common ["rankdir=TB;"]
