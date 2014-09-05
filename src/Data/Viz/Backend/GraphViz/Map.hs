776daa8f2011c3dca8b4c5932abc882e4d6125fe
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-05 02:00:45 -0400

    newlines

--- src/Data/Viz/Backend/GraphViz/Map.hs
+++ src/Data/Viz/Backend/GraphViz/Map.hs
@@ -7,20 +7,20 @@ module Data.Viz.Backend.GraphViz.Map (
 import qualified Data.Map as M
 
 mapTo'SimpleGraph :: String -> M.Map String [String] -> String
-mapTo'SimpleGraph t m = "digraph g {" ++ title t ++ concatenated ++ "}"
+mapTo'SimpleGraph t m = "digraph g {\n" ++ title t ++ concatenated ++ "\n}"
  where
   keys = M.keys m
   nodes =
    map
-    (\key -> map (\el -> "\""++key++"\"->\""++el++"\";") $ adjacencies key) keys
+    (\key -> map (\el -> "\""++key++"\"->\""++el++"\";\n") $ adjacencies key) keys
   adjacencies key =
    map
     (\elem -> elem) $ (M.!) m key
   concatenated = concat $ concat nodes
 
 listTo'SimpleGraph :: String -> [(String,[String])] -> String
-listTo'SimpleGraph t m = "digraph g {" ++ title t ++ concatenated ++ "}"
+listTo'SimpleGraph t m = "digraph g {\n" ++ title t ++ concatenated ++ "}\n"
  where
-  concatenated = concat $ concat $ map (\(key,rest) -> map (\el -> "\""++key++"\"->\""++el ++"\";") rest) m
+  concatenated = concat $ concat $ map (\(key,rest) -> map (\el -> "\""++key++"\"->\""++el ++"\";\n") rest) m
 
-title t = "graph [label=\""++ t ++"\", labelloc=t, fontsize=30];"
+title t = "graph [label=\""++ t ++"\", labelloc=t, fontsize=30];\n"
