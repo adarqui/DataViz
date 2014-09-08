6f2cdf1069dd27a62c0197e18f07d6c0e348b3f2
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-08 15:08:25 -0400

    slight cleanup and groupgraph

--- README.md
+++ README.md
@@ -30,3 +30,18 @@ import qualified Data.ByteString.Lazy.Char8 as B
 let g = simpleGraph $ B.unpack $ mapTo'FDG'JSON $ mapTo'FDG $ infra2014Map
 writeFile "/www/pub/test.js" g
 ```
+
+```
+:set -XOverloadedStrings
+import Data.Viz.Examples.Topologies
+import Data.Viz.Backend.GraphViz.ForceDirected
+import Data.Viz.Backend.Jit.ForceDirected.Example
+import qualified Data.ByteString.Lazy.Char8 as B
+let g = simpleGraph $ B.unpack $ mapTo'FDG'JSON $ mapTo'FDG $ infra2014Map
+writeFile "/www/pub/test.js" g
+```
+
+```
+writeFile "/tmp/k.dot" $ listTo'SimpleGraph "hello" lr [("hi",["hey","yo","lul"]),("hey",[]),("poop",["fffeeee-Fsdf"])]
+writeFile "/tmp/k.dot" $ listTo'GroupGraph "hello" lr [("poop","red")] [("poop",5.0)] [("hi",["hey"]),("hey",[]),("poop",["fffeeee-Fsdf"])]
+```
--- src/Data/Viz/Backend/GraphViz/Map.hs
+++ src/Data/Viz/Backend/GraphViz/Map.hs
@@ -2,6 +2,8 @@
 module Data.Viz.Backend.GraphViz.Map (
  mapTo'SimpleGraph,
  listTo'SimpleGraph,
+ mapTo'GroupGraph,
+ listTo'GroupGraph,
  lr,
  tb
 ) where
@@ -9,23 +11,32 @@ module Data.Viz.Backend.GraphViz.Map (
 import qualified Data.Map as M
 
 mapTo'SimpleGraph :: String -> String -> M.Map String [String] -> String
-mapTo'SimpleGraph t opts m = "digraph g {\n" ++ title t ++ opts ++ concatenated ++ "\n}"
- where
-  keys = M.keys m
-  nodes =
-   map
-    (\key -> map (\el -> "\""++key++"\"->\""++el++"\";\n") $ adjacencies key) keys
-  adjacencies key =
-   map
-    (\elem -> elem) $ (M.!) m key
-  concatenated = concat $ concat nodes
+mapTo'SimpleGraph t opts m = listTo'SimpleGraph t opts (M.toList m)
 
 listTo'SimpleGraph :: String -> String -> [(String,[String])] -> String
 listTo'SimpleGraph t opts m = "digraph g {\n" ++ title t ++ opts ++ concatenated ++ "}\n"
  where
-  concatenated = concat $ concat $ map (\(key,rest) -> map (\el -> "\""++key++"\"->\""++el ++"\";\n") rest) m
+  nodes =
+   map
+    (\(key,adjacencies) -> map (\adjacency -> "\""++key++"\"->\""++adjacency++"\";\n") adjacencies) m
+  concatenated = concat $ concat nodes
+
+
+mapTo'GroupGraph :: String -> String -> M.Map String String -> M.Map String Double -> M.Map String [String] -> String
+mapTo'GroupGraph t opts colors sizes m = listTo'GroupGraph t opts (M.toList colors) (M.toList sizes) (M.toList m)
+
+listTo'GroupGraph :: String -> String -> [(String,String)] -> [(String,Double)] -> [(String,[String])] -> String
+listTo'GroupGraph t opts colors sizes m = "digraph g {\n" ++ title t ++ opts ++ colors' ++ sizes' ++ concatenated ++ "\n}"
+ where
+  sizes' = concat $ map (\(group,size) -> "\""++group++"\" [size="++show size++"];\n") sizes 
+  colors' = concat $ map (\(group,color) -> "\""++group++"\" [style=filled, fillcolor ="++color++", color="++color++"];\n") colors
+  nodes =
+   map
+    (\(key,adjacencies) -> map (\el -> "\""++key++"\"->\""++el++"\";\n") adjacencies) m
+  concatenated = concat $ concat nodes
+
 
 title t = "graph [label=\""++ t ++"\", labelloc=t, fontsize=30];\n"
-common opts = concat $ map (++ "\n") $ ["overlap=false;","bgcolor=transparent;","splines=true;"] ++ opts
+common opts = concat $ map (++ "\n") $ ["overlap=false;","bgcolor=transparent;","splines=true;","splines=spline;"] ++ opts
 lr = common ["rankdir=LR;"]
 tb = common ["rankdir=TB;"]
